82635d00459cc950e3c78435c5fcb08665c33d52
Harsh28Pandey <harsh28.knp@gmail.com>
2026-05-11 17:21:39 +0530

    Update sendOtpMail.js

--- backend/emailVerify/sendOtpMail.js
+++ backend/emailVerify/sendOtpMail.js
@@ -4,7 +4,9 @@ dotenv.config();
 
 const sendOtpMail = async (email, otp) => {
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASSWORD
