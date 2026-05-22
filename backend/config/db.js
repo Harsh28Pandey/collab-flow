25ff29f68d00bee1c4a01a86c1917eda909200f4
Harsh28Pandey <harsh28.knp@gmail.com>
2026-05-22 11:24:29 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        console.log("MONGO URI:", process.env.MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI, {});
-        console.log("MongoDB Database Connected");
+        // console.log("MongoDB Database Connected");
     } catch (error) {
         console.error("Error connecting to MongoDB: ", error);
         // process.exit(1);
