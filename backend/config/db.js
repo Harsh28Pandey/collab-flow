e6ae06c1514a30801d338a112e55a6fa75a3288a
Harsh28Pandey <harsh28.knp@gmail.com>
2026-05-22 11:25:45 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,35 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
+
     try {
-        await mongoose.connect(process.env.MONGO_URI, {});
+
+        await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 30000
+        });
+
         // console.log("MongoDB Database Connected");
+
     } catch (error) {
-        console.error("Error connecting to MongoDB: ", error);
-        // process.exit(1);
+
+        console.error("Error connecting to MongoDB:", error);
+
+        process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
+
+// const mongoose = require("mongoose")
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI, {});
+//         // console.log("MongoDB Database Connected");
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB: ", error);
+//         // process.exit(1);
+//     }
+// }
+
+// module.exports = connectDB;
