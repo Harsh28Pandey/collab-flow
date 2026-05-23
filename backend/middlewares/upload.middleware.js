7f80761ea8170a6e03aaa640c21de17c32ac76ed
Harsh28Pandey <harsh28.knp@gmail.com>
2026-05-23 10:40:19 +0530

    Update upload.middleware.js

--- backend/middlewares/upload.middleware.js
+++ backend/middlewares/upload.middleware.js
@@ -6,25 +6,23 @@ const storage = multer.memoryStorage();
 //* file filter
 const fileFilter = (req, file, cb) => {
 
-    ```
-const allowedTypes = [
-    "image/jpeg",
-    "image/png",
-    "image/jpg",
-    "image/webp"
-];
+    const allowedTypes = [
+        "image/jpeg",
+        "image/png",
+        "image/jpg",
+        "image/webp"
+    ];
 
-if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-} else {
-    cb(
-        new Error(
-            "Only jpeg, jpg, png and webp are allowed"
-        ),
-        false
-    );
-}
-```
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(
+            new Error(
+                "Only jpeg, jpg, png and webp are allowed"
+            ),
+            false
+        );
+    }
 
 };
 
